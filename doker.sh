014f5ab80ffbbce86bef11f109527f3aaa11e4ab
Vinha <fernando.h.vinha@gmail.com>
2023-05-22 16:18:57 -0300

    adasads

--- doker.sh
+++ doker.sh
@@ -1,4 +1,25 @@
-docker-compose build
-docker-compose run dalai npx dalai alpaca install 7B # or a different model
-docker-compose up -d
+#!/bin/bash
+
+# Verifica se o Docker está instalado
+if ! command -v docker &> /dev/null; then
+    echo "Docker não está instalado. Instalando Docker..."
+    # Instala o Docker
+    sudo apt update
+    sudo apt install docker.io -y
+    # Adiciona o usuário atual ao grupo "docker"
+    sudo usermod -aG docker $USER
+    echo "Docker foi instalado com sucesso."
+fi
+
+# Verifica se o Docker Compose está instalado
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose não está instalado. Instalando Docker Compose..."
+    # Instala o Docker Compose
+    sudo apt install docker-compose -y
+    echo "Docker Compose foi instalado com sucesso."
+fi
+
+# Executa o comando "dalai alpaca install 7B"
+echo "Executando 'dalai alpaca install 7B'..."
+docker-compose run dalai npx dalai alpaca install 7B
 
