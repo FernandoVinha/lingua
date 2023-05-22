d9d0576563fdf307c38f6554b51e4abef15b9685
Vinha <fernando.h.vinha@gmail.com>
2023-05-22 16:13:27 -0300

    doke

--- doker.sh
+++ doker.sh
@@ -1,4 +1,4 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+docker-compose build
+docker-compose run dalai npx dalai alpaca install 7B # or a different model
+docker-compose up -d
 
